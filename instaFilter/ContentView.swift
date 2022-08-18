1710ffa8b67f8f2a6fa64baed1e46aeb23a8c94c
adarsh <adarsh0ranjan@gmail.com>
2022-08-18 08:11:51 +0530

    Initial Commit

---
+++ instaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instaFilter
+//
+//  Created by Roro Solutions on 18/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instaFilter/instaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaFilterApp.swift
+//  instaFilter
+//
+//  Created by Roro Solutions on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct instaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
